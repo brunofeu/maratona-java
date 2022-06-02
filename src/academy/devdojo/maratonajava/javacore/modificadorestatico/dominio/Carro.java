036dd9483747b0c39a6c7d065497dc2f62bed0fa
brunofeu <brunofeu89@gmail.com>
2022-06-02 16:31:37 -0300

    metodos estaticos

---
+++ src/academy/devdojo/maratonajava/javacore/modificadorestatico/dominio/Carro.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.modificadorestatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double valocidadeMaxima;
+    private static double velocidadeLimite = 250;
+    //por ser static, é um atributo da classe carro, e não mais de cada elemento
+    //se for alterado no código c1.velocidadeMaxima, todos os carros seguintes instanciados
+    //terão o novo valor de velocidadeMaxima
+
+    public Carro(String nome, double valocidadeMaxima) {
+        this.nome = nome;
+        this.valocidadeMaxima = valocidadeMaxima;
+    }
+
+    public void imprime(){
+        System.out.println("--------------");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Velocidade máxima: " + this.valocidadeMaxima);
+        System.out.println("Velocidade limite: " + Carro.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValocidadeMaxima() {
+        return valocidadeMaxima;
+    }
+
+    public void setValocidadeMaxima(double valocidadeMaxima) {
+        this.valocidadeMaxima = valocidadeMaxima;
+    }
+}
