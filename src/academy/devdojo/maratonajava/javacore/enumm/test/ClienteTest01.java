42b435ad979370dc192ea590bfdc7e0c03574e29
brunofeu <brunofeu89@gmail.com>
2022-06-03 17:02:26 -0300

    Enum

--- src/academy/devdojo/maratonajava/javacore/enumm/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/enumm/test/ClienteTest01.java
@@ -6,7 +6,7 @@ import academy.devdojo.maratonajava.javacore.enumm.dominio.TipoCliente;
 public class ClienteTest01 {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
