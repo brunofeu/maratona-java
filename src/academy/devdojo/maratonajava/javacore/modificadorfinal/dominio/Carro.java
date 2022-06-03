e9c35fb325e43d6109bfbf5019066280c57e04fe
brunofeu <brunofeu89@gmail.com>
2022-06-03 16:27:58 -0300

    modificador final tipo referencia

--- src/academy/devdojo/maratonajava/javacore/modificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/modificadorfinal/dominio/Carro.java
@@ -5,5 +5,11 @@ public class Carro {
     public static final double VELOCIDADE_LIMITE = 250;
     // quando substituido por "final" o valor não pode mais ser alterado
 
+    private final Comprador COMPRADOR = new Comprador();
+    // o COMPRADOR fica limitado a apenas uma instancia
+    // pode ser utilizado para abrir conexão com o banco de dados.
+    // não é necessário cada novo usuário inscanticar uma nova classe
+    // singleton pattern
+
 
 }
