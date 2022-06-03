225613ad6cbc944c898bf0b0bcc6dbe1ec2e1a05
brunofeu <brunofeu89@gmail.com>
2022-06-03 16:54:33 -0300

    Enum

---
+++ src/academy/devdojo/maratonajava/javacore/enumm/dominio/TipoCliente.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.enumm.dominio;
+
+public enum TipoCliente {
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+    // enumeração. bom para forçar o tipo que vai ser utilizado
+}
---
+++ src/academy/devdojo/maratonajava/javacore/enumm/test/ClienteTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.enumm.test;
+
+import academy.devdojo.maratonajava.javacore.enumm.dominio.Cliente;
+import academy.devdojo.maratonajava.javacore.enumm.dominio.TipoCliente;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
+        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+
+    }
+}
